b745e421d2562959ab555197534e8133cb9ceab8
albalopes <alba.lopes@ifrn.edu.br>
2026-06-19 16:35:09 +0000

    Acesso a Banco de Dados

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template
+import json
+from utils import db
+import os
+from flask_migrate import Migrate
+from models import Usuario
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db_usuario = os.getenv('DB_USERNAME')
+db_senha = os.getenv('DB_PASSWORD')
+db_mydb = os.getenv('DB_DATABASE')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+
+conexao = f"mysql+pymysql://{db_usuario}:{db_senha}@{db_host}:{db_port}/{db_mydb}"
+print(conexao)
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
